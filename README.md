57e0732d44a331d8a8e6341f0accc64bd65d00d9
juniorpen01 <01juniorpen@gmail.com>
2025-12-15 21:05:43 +0800

    Learn numbers, strings, and operators; add learnxinyminutes to references

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # learn-js
+
 An account of my preliminary learnings of JavaScript.
+
+## References
+
+1. <https://learnxinyminutes.com/javascript/>
--- src/main.js
+++ src/main.js
@@ -13,4 +13,59 @@
   let bar = "idiot";
   console.log(Math.abs(Number(bar)));
   console.log(+bar);
+  console.log(undefined);
+}
+
+// set this global for ez printing
+const p = console.log;
+
+p("numbers, strings, and operators");
+{
+  p(Infinity);
+  p(+Infinity);
+  p(-Infinity);
+  p(NaN);
+  p(Infinity + Infinity - Infinity);
+
+  p(true);
+  p(false);
+  p(!true);
+  p(!false);
+
+  let foo,
+    bar = 1;
+  p(foo);
+  p(bar);
+
+  let baz = 1,
+    qux = 2;
+  p(baz == qux);
+  p(baz === qux);
+  p(baz != qux);
+  p(baz !== qux);
+
+  p("1, 2, " + 3);
+  p("1, 2, " + [3, 4]);
+
+  // p(13 + !0);
+  p(13 + Number(!0));
+  p("13" + !0);
+
+  // p(5 == "5");
+  // p("5" == 5);
+  p(null == undefined);
+
+  // p(5 === "5");
+  // p("5" === 5);
+  p(null === undefined);
+
+  p("a"[1]);
+  p("a".charAt(1));
+
+  p("a".substring(0, 3));
+
+  p("asdf".length);
+
+  foo = "asdf";
+  // foo.length = 1;
 }
