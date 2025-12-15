966baa62810a88a155a8da92bf7f58b8d2708b8c
juniorpen01 <01juniorpen@gmail.com>
2025-12-15 20:43:56 +0800

    Add type checking; preliminary tests for writing js

---
+++ src/main.js
@@ -0,0 +1,16 @@
+// main.js
+
+// some fun or whatever
+{
+  /**
+   * what
+   * is
+   * ts
+   * */
+  let foo = console.log("hello world");
+  foo;
+
+  let bar = "idiot";
+  console.log(Math.abs(Number(bar)));
+  console.log(+bar);
+}
